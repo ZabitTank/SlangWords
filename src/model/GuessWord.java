850916d8a8c6ad785c6ad9a55f3edd769e52175e
RabitTank <bestmatch236@gmail.com>
2021-12-24 16:16:53 +0700

    - GuessWord class: using to hold variable for game - Update SlangWords: delete, random GuessWord function

---
+++ src/model/GuessWord.java
@@ -0,0 +1,19 @@
+package model;
+
+import java.util.Vector;
+
+public class GuessWord {
+
+        public String word;
+        public String mean;
+        public String trueAnswer;
+        public Vector<String> availableAnswer;
+
+        public GuessWord(String word, String mean, Vector<String> block, int index){
+            this.word = word;
+            this.mean = mean;
+            this.trueAnswer = block.elementAt(index);
+            this.availableAnswer = block;
+        }
+
+}
--- src/model/SlangWords.java
+++ src/model/SlangWords.java
@@ -125,6 +125,9 @@ public class SlangWords {
     }
 
     // methods
+
+
+
     /**
      * Description: saveDataStructure methods used to write data in file with ObjectOutputStream
      */
@@ -151,6 +154,159 @@ public class SlangWords {
         }
     }
 
+    /**
+     *
+     * Description: get means by value. return Array of possible means
+     * @param word: is a String
+     */
 
+    public Vector<String> getValue(String word){
+        Vector<String> result = new Vector<>();
+
+        String s;
+        if((s = data.get(word))!= null) {
+            result.add(s);
+
+            int i = 1;
+            while ((s = data.get(word + ":::" + i)) != null) {
+                result.add(s);
+                i++;
+            }
+        }
+        return result;
+    }
+
+    public Vector<String> getWord(String mean){
+        Vector<String> result = new Vector<>();
+        String s;
+
+        if((s = reverseData.get(mean))!= null) {
+            result.add(s);
+
+            int i = 1;
+            while ((s = reverseData.get(mean + ":::" + i)) != null) {
+                result.add(s);
+                i++;
+            }
+        }
+        return result;
+    }
+
+    public void addSlangWord(int choose,String word, String mean, int index){
+        if(choose == REPLACE) {
+            String newWord = word;
+            if(index != 0) {
+                newWord = word +":::" + index;
+            }
+            data.put(newWord,mean);
+
+        }
+        else if (choose == DUPLICATE){
+            data.put(word+":::"+(index),mean);
+        }
+
+        if(!reverseData.containsKey(mean)) {
+            reverseData.put(mean,word);
+        }
+        else {
+            int index2 =1;
+            while(reverseData.containsKey(mean+":::"+index2)) index2++;
+            reverseData.put(mean+":::"+index2,word);
+        }
+    }
+
+    public void delete(String word,int index){
+        String mean = this.data.get(word);
+
+        // delete in data
+
+        // find last index
+        int lastIndex =1;
+        while(data.containsKey(word+":::"+lastIndex)) lastIndex++;
+        lastIndex--;
+        if(lastIndex ==0) this.data.remove(word);
+        else {
+            if(index ==0) {
+                data.put(word,data.get(word+":::"+1));
+                index++;
+            }
+            for(int i = index; i < lastIndex;i++){
+                data.put(word+":::"+i,data.get(word+":::"+(i+1)));
+            }
+            data.remove(word+":::"+lastIndex);
+        }
+
+        // delete reverseData
+
+        // find how many mean(n)
+        int index2=0;
+        lastIndex=1;
+
+        while(reverseData.containsKey(mean+":::"+lastIndex)) {
+            if(reverseData.get(mean+":::"+lastIndex).equals(word))
+                index2 = lastIndex;
+            lastIndex++;
+        }
+        lastIndex--;
+        if(lastIndex==0) {
+            reverseData.remove(mean);
+        }
+        else{
+            if(index2==0) {
+                reverseData.put(mean,reverseData.get(mean+":::"+1));
+                index2++;
+            }
+            for(int i =index2;i< lastIndex;i++) {
+                reverseData.put(mean + ":::" + i, reverseData.get(mean + ":::" +(i + 1)));
+            }
+            reverseData.remove(mean+":::"+lastIndex);
+        }
+    }
+
+    public boolean isExist(String word , String mean){
+        if(data.containsKey(word)) {
+            Vector<String> allMean = this.getValue(word);
+            if(allMean.isEmpty()) return false;
+            return allMean.contains(mean);
+        }
+        return false;
+    }
+
+    public GuessWord guess(boolean wordOrMean){
+        Random random = new Random();
+
+        List<String> questions = null;
+        String question=null;
+        Vector<String> trueAnswers = null;
+
+        if(wordOrMean == GUESS_WORD) {
+            questions = new ArrayList<>(reverseData.values());
+            question = questions.get(random.nextInt(questions.size()));
+            trueAnswers = this.getValue(question);
+        }
+        else if(wordOrMean == GUESS_MEAN) {
+            questions = new ArrayList<>(data.values());
+            question = questions.get(random.nextInt(questions.size()));
+            trueAnswers = this.getWord(question);
+        }
+
+        String trueAnswer = trueAnswers.elementAt(random.nextInt(trueAnswers.size()));
+
+        Vector<String> block = new Vector<>();
+
+        int i = 0;
+        while(i < 3) {
+            String randomValue = questions.get(random.nextInt(questions.size()));
+            if(!randomValue.equals(trueAnswer)) {
+                block.add(randomValue);
+                i++;
+            }
+        }
+
+        int index = random.nextInt(4);
+        block.insertElementAt(trueAnswer,index);
+
+        return new GuessWord(question,question,block,index);
+    }
 
 }
